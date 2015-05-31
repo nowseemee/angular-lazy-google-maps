ef0b08a802ba9cd19e2528fbe397bc9edbcd5da9
Daniel Beck <nowseemee@gmail.com>
2015-05-31 19:14:34 +0200

    add more tests

--- src/map/map.directive.spec.js
+++ src/map/map.directive.spec.js
@@ -3,8 +3,16 @@
 
     describe('Unit testing lazyGoogleMaps', function() {
         var $compile,
-            $rootScope;
-
+            $rootScope,
+            lazyGoogleMaps = '<lazy-google-maps ' +
+            'lat="location.lat" ' +
+            'lng="location.lng" ' +
+            'country="location.country" ' +
+            'city="location.city" ' +
+            'place="location.place" ' +
+            'ng-init="location = ' +
+            '{lat: -34.397, lng: 150.644, country: \'Australia\', city: \'Sydney\', place: \'dunno\'}"> ' +
+            '</lazy-google-maps>';
         beforeEach(function () {
             module('lazyGoogleMaps');
             module('templates');
@@ -15,16 +23,31 @@
             $rootScope = _$rootScope_;
         }));
 
+        it('should append the google maps script tag to the document', function() {
+            var element = angular.element(lazyGoogleMaps);
+            var googleMapsScriptTag;
+
+            $compile(element)($rootScope);
+            $rootScope.$digest();
+
+            googleMapsScriptTag = document.querySelector('script[src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=mapLoaded&libraries=places"]');
+
+            expect(googleMapsScriptTag).toBeDefined();
+        });
+
+        it('should render the map with scope.$id', function() {
+            var element = angular.element(lazyGoogleMaps);
+            var isoScope;
+
+            var compiledElement = $compile(element)($rootScope);
+            $rootScope.$digest();
+            isoScope = element.isolateScope();
+
+            expect(compiledElement.html()).toContain('lazy-google-maps-search-' + isoScope.$id);
+            expect(compiledElement.html()).toContain('lazy-google-maps-canvas-' + isoScope.$id);
+        });
+
         it('should have the scope values', function() {
-            var lazyGoogleMaps = '<lazy-google-maps ' +
-                'lat="location.lat" ' +
-                'lng="location.lng" ' +
-                'country="location.country" ' +
-                'city="location.city" ' +
-                'place="location.place" ' +
-                'ng-init="location = ' +
-                '{lat: -34.397, lng: 150.644, country: \'Australia\', city: \'Sydney\', place: \'dunno\'}"> ' +
-                '</lazy-google-maps>';
             var element = angular.element(lazyGoogleMaps);
             var isoScope;
 
